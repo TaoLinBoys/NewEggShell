2b47380c1c6d9befaca6559fe1ded71774089e4e
Harry Lum <hlum1@stuy.edu>
2016-11-21 15:11:05 -0500

    start

---
+++ myShell.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char * s = (char*)malloc(sizeof(s));
+    char * argv[100];
+    
+    while (1){
+        fgets(s, sizeof(s), stdin){
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+}
